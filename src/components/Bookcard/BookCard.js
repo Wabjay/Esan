9d056a09731d63fecdc5a350274521a2d8b9c96f
wabjay <jefferyjohn230@gmail.com>
2024-07-25 11:13:59 +0100

    Modify server call

--- src/components/Bookcard/BookCard.js
+++ src/components/Bookcard/BookCard.js
@@ -4,29 +4,32 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 import Download from '../../images/download.svg';
 
 const BookCard = ({ pdf, content, id }) => {
-  const downloadPDF = async () => {
-    try {
-      const response = await fetch(pdf);
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = `${content}.pdf`; // Set the default file name
-      document.body.appendChild(a);
-      a.click();
-      a.remove();
-      window.URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error('Error downloading the PDF:', error);
-    }
-  };
+//   const downloadPDF = async () => {
+//     try {
+//       const response = await fetch(pdf);
+//       const blob = await response.blob();
+//       const url = window.URL.createObjectURL(blob);
+//       const a = document.createElement('a');
+//       a.href = url;
+//       a.download = `${content}.pdf`; // Set the default file name
+//       document.body.appendChild(a);
+//       a.click();
+//       a.remove();
+//       window.URL.revokeObjectURL(url);
+//     } catch (error) {
+//       console.error('Error downloading the PDF:', error);
+//     }
+//   };
 
   return (
     <div className="bookCard">
       <p className="book">{content}</p>
-      <button className="pdfBorder" onClick={downloadPDF}>
+      {/* <button className="pdfBorder" onClick={downloadPDF}>
         <img src={Download} alt="download button" />
-      </button>
+      </button> */}
+      <a target="_blank" href={pdf} className="pdfBorder" rel="noreferrer">
+          <img src={Download} alt="download button" />
+       </a>
     </div>
   );
 };
